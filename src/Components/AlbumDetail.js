8dec8cfc3a7a55ebf640d25d03076fda40217393
DominikColl <DAColl@student.fullsail.edu>
2019-11-18 15:09:28 -0500

    More details now show for everything

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { loadAlbumDetail } from '../Page/Code'
-
-
+import './style.css';
+import Header from './header';
 const AlbumDetail = (props) => {
 
   let { albumId } = useParams();
-
+  //artist playcount published summary tracksonalbum
   let [albumData, setAlbumData] = useState({})
+  let [artist, setArtist] = useState('')
+  let [playCount, setPlayCount] = useState('')
+  let [published, setPublished] = useState('')
+  let [summary, setSummary] = useState('')
+  let [tracks, setTracks] = useState({})
 
   useEffect(() => {
     console.log(albumId);
     loadAlbumDetail(albumId).then((d) => {
       setAlbumData(d.album);
+      setArtist(d.album.artist);
+      setPlayCount(d.album.playCount);
+      setPublished(d.album.wiki.published);
+      setSummary(d.album.wiki.summary);
+      setTracks(d.album.tracks.track)
       console.log(d.album);
     })
   }, [albumId])
@@ -20,11 +30,17 @@ const AlbumDetail = (props) => {
 
 
   return (
-    <div>
+    <div class='moreDetail'>
+      <Header />
       <h1>I am Album Detail.</h1>
       <p>{albumId}</p>
       <ul>
         <li>{albumData.name}</li>
+        <li>{artist}</li>
+        <li>{playCount}</li>
+        <li>{published}</li>
+        <li>{summary}</li>
+        {/* <li>{tracks}</li> */}
       </ul>
     </div>
   )
--- src/Components/ArtistDetail.js
+++ src/Components/ArtistDetail.js
@@ -1,30 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { loadArtistDetail } from '../Page/Code'
-
+import './style.css';
+import Header from './header';
 
 const SongDetail = (props) => {
 
     let { artistId } = useParams();
+    //name bio.summary stats.playcount similar.artist[]
 
-    let [albumData, setAlbumData] = useState({})
-
+    let [artistName, setArtistName] = useState('');
+    let [bio, setBio] = useState('');
+    let [playCount, setPlayCount] = useState('');
+    let [similarArt, setSimilarArt] = useState({});
     useEffect(() => {
         console.log(artistId);
         loadArtistDetail(artistId).then((d) => {
-            // setAlbumData(d.album);
-            console.log(d);
+            console.log(d.artist);
+            setArtistName(d.artist.name);
+            setBio(d.artist.bio.summary);
+            setPlayCount(d.artist.stats.playcount);
+            setSimilarArt(d.artist.similar.artist);
         })
     }, [artistId])
 
 
 
     return (
-        <div>
+        <div class='moreDetail'>
+            <Header />
             <h1>I am Album Detail.</h1>
             <p>{artistId}</p>
             <ul>
-                {/* <li>{albumData.name}</li> */}
+                <li>{artistName}</li>
+                <li>{bio}</li>
+                <li>{playCount}</li>
+                {/* <li>{similarArt}</li> */}
             </ul>
         </div>
     )
