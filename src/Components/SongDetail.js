8ceaf3a7d25f02f7963fc93e883e406d90d97f29
DominikColl <DAColl@student.fullsail.edu>
2019-11-18 14:45:19 -0500

    Song detail now displays more info about song

--- src/Components/SongDetail.js
+++ src/Components/SongDetail.js
@@ -1,30 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { loadSongDetail } from '../Page/Code'
-
+import './style.css';
+import Header from './header'
 
 const SongDetail = (props) => {
 
     let { songId } = useParams();
 
-    let [albumData, setAlbumData] = useState({})
-
+    let [songData, setSongData] = useState([])
+    let [artistName, setArtistName] = useState('');
+    let [albumTitle, setAlbumTitle] = useState('');
+    let [playCount, setPlayCount] = useState('');
+    let [songBio, setSongBio] = useState('');
     useEffect(() => {
         console.log(songId);
         loadSongDetail(songId).then((d) => {
-            // setAlbumData(d.album);
-            console.log(d);
+            console.log(d.track);
+            setSongData(d.track);
+            setArtistName(d.track.artist.name);
+            setAlbumTitle(d.track.album.title);
+            setPlayCount(d.track.playcount);
+            setSongBio(d.track.wiki.summary);
+            console.log(d.track.artist.name);
+        }).then((e) => {
+            console.log(songData);
         })
+
     }, [songId])
-
-
-
     return (
-        <div>
+        <div class='moreDetail'>
+            <Header />
             <h1>I am Album Detail.</h1>
             <p>{songId}</p>
             <ul>
-                {/* <li>{albumData.name}</li> */}
+                {/* name artist release date summary playcount album[title */}
+                <li>{songData.name}</li>
+                <li>{artistName}</li>
+                <li>Album: {albumTitle}</li>
+                <li>Play Count: {playCount}</li>
+                <li>Summary: {songBio}</li>
             </ul>
         </div>
     )
